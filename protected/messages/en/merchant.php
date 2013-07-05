ac1113d8ca4139f867fadc2e3985f41b253f6d55
Wei Chen <yweichen@gmail.com>
2013-07-06 04:23:38 +0800

    Merchant language file

---
+++ protected/messages/en/merchant.php
@@ -0,0 +1,27 @@
+<?php
+
+return array(
+    'FORM_TITLE_SIGNUP' => 'Open Restaurant with 3atnow',
+    
+    'BUTTON_SIGNUP' => 'JOIN NOW',
+    
+    'LABEL_MERCHANT' => 'Merchant',
+    'LABEL_RESTAURANT' => 'Restaurant Name',
+    'LABEL_RESTAURANT_URL' => 'Restaurant URL',
+    'LABEL_CONTACT_PERSON_FIRST_NAME' => 'First Name',
+    'LABEL_CONTACT_PERSON_LAST_NAME' => 'Last Name',
+    'LABEL_CONTACT_NUMBER_COUNTRY' => 'Country',
+    'LABEL_CONTACT_NUMBER' => 'Mobile Number',
+    'LABEL_LOGIN_EMAIL' => 'Login Email',
+    'LABEL_LOGIN_PASSWORD' => 'Password',
+    'LABEL_SIGNUP_DATE' => 'Joined Date',
+    'LABEL_SIGNUP_IP' => 'Joined IP',
+    
+    
+    
+    'ERROR_EMAIL_MISSING' => 'Please enter a valid {attribute}.',
+    'ERROR_SHORT_INPUT' => '{attribute} is too short. At least {min} characters.',
+    'ERROR_EMPTY_INPUT' => 'Please fill in {attribute}.',
+    'ERROR_NUMERIC_INPUT' => '{attribute} must be a number.',
+    
+);
