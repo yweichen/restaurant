4ca5210b135e3db6b1be033bc64c82fb1b281ab3
Wei Chen <yweichen@gmail.com>
2013-07-06 04:27:17 +0800

    General Language file

---
+++ protected/messages/en/general.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'LABEL_TOS' => 'I agree to the ' . CHtml::link('Terms & Conditions', array('merchant/join'), array('target'=>'_blank')),
+    
+    'ERROR_AGREE_TOS' => 'To register you must accept the terms & conditions.',
+);
