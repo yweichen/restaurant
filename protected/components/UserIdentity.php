f038b5ea6de59ab3b0f0fe28fa1fe68b71a67f3b
Wei Chen <yweichen@gmail.com>
2013-07-06 04:32:59 +0800

    Add blowfishSalt method

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -5,29 +5,52 @@
  * It contains the authentication method that checks if the provided
  * data can identity the user.
  */
-class UserIdentity extends CUserIdentity
-{
-	/**
-	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
-	 * @return boolean whether authentication succeeds.
-	 */
-	public function authenticate()
-	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
-	}
+class UserIdentity extends CUserIdentity {
+
+    /**
+     * Authenticates a user.
+     * The example implementation makes sure if the username and password
+     * are both 'demo'.
+     * In practical applications, this should be changed to authenticate
+     * against some persistent user identity storage (e.g. database).
+     * @return boolean whether authentication succeeds.
+     */
+    public function authenticate() {
+        $users = array(
+            // username => password
+            'demo' => 'demo',
+            'admin' => 'admin',
+        );
+        if (!isset($users[$this->username]))
+            $this->errorCode = self::ERROR_USERNAME_INVALID;
+        elseif ($users[$this->username] !== $this->password)
+            $this->errorCode = self::ERROR_PASSWORD_INVALID;
+        else
+            $this->errorCode = self::ERROR_NONE;
+        return !$this->errorCode;
+    }
+
+    /**
+     * Generate a random salt in the crypt(3) standard Blowfish format.
+     *
+     * @param int $cost Cost parameter from 4 to 31.
+     *
+     * @throws Exception on invalid cost parameter.
+     * @return string A Blowfish hash salt for use in PHP's crypt()
+     */
+    public static function blowfishSalt($cost = 13) {
+        if (!is_numeric($cost) || $cost < 4 || $cost > 31) {
+            throw new Exception("cost parameter must be between 4 and 31");
+        }
+        $rand = array();
+        for ($i = 0; $i < 8; $i += 1) {
+            $rand[] = pack('S', mt_rand(0, 0xffff));
+        }
+        $rand[] = substr(microtime(), 2, 6);
+        $rand = sha1(implode('', $rand), true);
+        $salt = '$2a$' . sprintf('%02d', $cost) . '$';
+        $salt .= strtr(substr(base64_encode($rand), 0, 22), array('+' => '.'));
+        return $salt;
+    }
+
 }
